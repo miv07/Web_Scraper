2842e0bb9b69f14568932173b85b9d2ff806e3ca
miv07 <esthercharlemagne@gmail.com>
2024-11-27 20:06:36 -0800

    Adds CLI for user input

--- Scrapper.py
+++ Scrapper.py
@@ -1,11 +1,25 @@
 import requests
+import argparse
+import sys
 
-# Making a GET request
-r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+# create a parser object
+parser = argparse.ArgumentParser(description = "A web scrapper program")
+
+# add argument
+parser.add_argument('-u', '--url', type=str, help = "The argument is the webpage to be scraped")
+
+# parse the arguments from standard input
+args = parser.parse_args()
+
+# make GET request
+response = requests.get(args.url)
+
+# print url
+print(response.url)
 
 # check status code for response received
 # success code - 200
-print(r)
+print(response.status_code)
 
-# print content of request
-print(r.content)
+# print content of request as text
+print(response.text)
